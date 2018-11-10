f394902e302273ccdd367b0e742c749af352151d
MilenaFigueres <figueresmilena@gmail.com>
2018-11-09 21:34:19 -0300

    Add styles and Complain-Add_complain

--- parking_app/models.py
+++ parking_app/models.py
@@ -1,29 +1,44 @@
 from django.db import models
 from django.utils import timezone
+import uuid 
 
 class Own(models.Model):
-	name = models.CharField(max_length=70)
-	address = models.CharField(max_length=150, default='')
-	location = models.CharField(max_length=150, default='')
-	city = models.CharField(max_length=150, default='Mendoza')
-	slack = models.CharField(max_length=150, default='')
+    name = models.CharField(max_length=70)
+    address = models.CharField(max_length=150, default='')
+    location = models.CharField(max_length=150, default='')
+    city = models.CharField(max_length=150, default='Mendoza')
+    slack = models.CharField(max_length=150, default='')
+    celular = models.IntegerField(default=0)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 class Vehicle(models.Model):
-	own = models.ForeignKey(
+    own = models.ForeignKey(
         Own,
         on_delete=models.CASCADE
     )
-	patent = models.CharField(max_length=10)
-	created_date = models.DateTimeField(
-            default=timezone.now)
-	color = models.CharField(max_length=50, default='')
-	model = models.CharField(max_length=150, default='')
+    patent = models.CharField(max_length=10)
+    color = models.CharField(max_length=50, default='')
+    model = models.CharField(max_length=150, default='')
 
-	def __str__(self):
-		return self.patent
+    def __str__(self):
+        return self.patent
 
+
+class Complain(models.Model):
+    own = models.ForeignKey(
+        Own,
+        on_delete=models.CASCADE
+    )
+    vehicle = models.ForeignKey(
+        Vehicle,
+        on_delete=models.CASCADE
+    )
+    created_date = models.DateTimeField(
+        default=timezone.now)
+
+    def __str__(self):
+        return uuid.uuid4().hex[:6].upper()
 
